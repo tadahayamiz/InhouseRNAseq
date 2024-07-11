e7835e046130b39600a1e40809278b1471f447ab
tadahayamiz <tadahaya@gmail.com>
2024-07-11 22:22:51 +0900

    main test

--- main.sh
+++ main.sh
@@ -115,11 +115,11 @@ parent=`dirname ${work_dir}`
 # pushd ${work_dir}
 
 q1=()
-for f1 in "${work_dir}*_1.*"; do
-  q1+=(${f1})
+for f1 in "${work_dir}"*_1.*; do
+  q1+=("${f1}")
 done
-for f2 in "${work_dir}*_2.*"; do
-  q2+=(${f2})
+for f2 in "${work_dir}"*_2.*; do
+  q2+=("${f2}")
 done
 
 echo ${q1[@]}
